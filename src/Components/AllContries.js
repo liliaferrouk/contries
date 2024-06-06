4d42c4703402d519744b28981e0ccbbdee93f1cb
liliaferrouk <ferrouklilia@gmail.com>
2024-06-06 20:37:34 +0200

    lazy load done

--- src/Components/AllContries.js
+++ src/Components/AllContries.js
@@ -11,9 +11,10 @@ function AllContries({darkMode,usedData,setSelectedContrie}) {
     const [filteredCountries, setFilteredCountries] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [loading, setLoading] = useState(false);
-    const countriesPerPage = 10;
+    const [hasMore,setHasMore] = useState(true)
+    const countriesPerPage = 5;
 
-    const fetchCountries = async (page) => {
+    const fetchCountries = async (page, reset = false) => {
         setLoading(true);
         const startIndex = (page - 1) * countriesPerPage;
         const endIndex = startIndex + countriesPerPage;
@@ -28,18 +29,28 @@ function AllContries({darkMode,usedData,setSelectedContrie}) {
                 resolve(dataSlice);
             }, 500); // Simulate delay for demonstration purposes (you can remove this in your actual code)
         });
-        setFilteredCountries(prevCountries => [...prevCountries, ...result]);
+        setFilteredCountries(prevCountries => reset ? result : [...prevCountries, ...result]);
+        setHasMore(result.length === countriesPerPage);
         setLoading(false);
     };
 
     useEffect(() => {
-        fetchCountries(currentPage);
-    }, [currentPage, searchQuery, filter]);
+        setFilteredCountries([]);
+        setCurrentPage(1);
+        fetchCountries(1, true);
+    }, [searchQuery, filter]);
+
+
+    useEffect(() => {
+        if (currentPage > 1) {
+            fetchCountries(currentPage);
+        }
+    }, [currentPage]);
 
     const handleScroll = () => {
         const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-        console.log("Scroll detected. ScrollTop:", scrollTop, "ClientHeight:", clientHeight, "ScrollHeight:", scrollHeight);
-        if (scrollTop + clientHeight > scrollHeight -50 && !loading) {
+        // console.log("Scroll detected. ScrollTop:", scrollTop, "ClientHeight:", clientHeight, "ScrollHeight:", scrollHeight);
+        if (scrollTop + clientHeight > scrollHeight -50 && !loading && hasMore) {
             setCurrentPage(prevPage => prevPage + 1);
         }
     };
@@ -47,7 +58,7 @@ function AllContries({darkMode,usedData,setSelectedContrie}) {
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+    }, [loading, hasMore]);
 
   return (
     <div>
@@ -79,6 +90,7 @@ function AllContries({darkMode,usedData,setSelectedContrie}) {
                 />
             ))}
             {loading && <p>Loading...</p>}
+            {!loading && filteredCountries.length ===0 && <p>No countries found.</p>}
         </div>
     </div>
   )
